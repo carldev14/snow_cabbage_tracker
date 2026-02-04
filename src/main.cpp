91eedfca8cba7aa24ce2047a04a929955fe4afde
carldev14 <carldev14.ac@gmail.com>
2026-02-05 00:10:15 +0800

    Modified: Removed the instances inside the main.cpp. All intances are defined inside the "system_manager.cpp"

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,38 @@
-#include <Arduino.h>
-#include <WiFi.h>
-#include "system/wifi_manager.h"
-#include "system/server_manager.h"
-// #include "config/pins_manager.h"
-#include "_sensors_manager.h"
-
-// Create instances
-WiFiManager wifi;
-ServerManager server;
-// PinsManager PinsManager;
-SensorsManager sensors;
-void initializeSystem()
-{
-    // Initialize WiFi, Server, and Pins
-    wifi.begin();
-    server.begin();
-    sensors.begin();
-    // Display initialization completed
-    Serial.println("System Initialized.");
-}
+#include "config/system_config.h"
+#include "manager/system/wifi_manager.h"
+#include "manager/sensors_manager.h"
 
 void setup()
 {
-    delay(1000);
-    Serial.begin(9600);                               // Add baud rate
-    Serial.println("\n=== Snow Cabbage Tracker ==="); // Display in the monitor the Plant Tracker
-    initializeSystem();                               // run initialization
+    Serial.begin(9600); //* Start Serial communication
+    SystemConfig::get().begin();       //* Initializes the system
+    Serial.println("Waiting for all systems to initialize...");
+    
+    //* Small delay to ensure everything is set up
+    delay(2000);
+
+    //* Indicate initialization complete
+    Serial.println("Initialization complete.");
+    Serial.println("\n=== Plant Tracker ==="); //* Display in the monitor the Plant Tracker
 }
 
 void loop()
 {
+    //? Run the WiFi manager update
     wifi.update();
-    // String temp = sensors.getTemp();
-    // String Humi = sensors.getHumi();
-    // String moist = sensors.getSoilMoistData(1);
-    // Serial.println("moist:");
-    // Serial.println(moist);
-    // Serial.println("temp:");
-    // Serial.println(temp);
-    // Serial.println("humi:");
-    // Serial.println(Humi);
-    // bool isNight = sensors.isNightDetected();
-    // bool isRain = sensors.isRainDetected();
-    // Serial.println(isRain ? "Raining" : "Clear");
-    // Serial.println(isNight);
-    // Add your sensor reading logic here
+    //* String temp = sensors.getTemp();
+    //* String Humi = sensors.getHumi();
+    //* String moist = sensors.getSoilMoistData(1);
+    //* Serial.println("moist:");
+    //* Serial.println(moist);
+    //* Serial.println("temp:");
+    //* Serial.println(temp);
+    //* Serial.println("humi:");
+    //* Serial.println(Humi);
+    //* bool isNight = sensors.isNightDetected();
+    //* bool isRain = sensors.isRainDetected();
+    //* Serial.println(isRain ? "Raining" : "Clear");
+    //* Serial.println(isNight);
+    //* Add your sensor reading logic here
     delay(1000);
 }
