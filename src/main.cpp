408c143eca0e1a8fdd8247e344eed41b4910f1ab
carldev14 <carldev14.ac@gmail.com>
2026-02-08 16:11:31 +0800

    Make the code cleaner: Use the mainLogic for effectively managing functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,23 @@
+#include "Arduino.h"
 #include "config/system_config.h"
 #include "manager/system/wifi_manager.h"
-#include "manager/sensors_manager.h"
+#include "manager/logic_manager.h"
+
+LogicManager logic;
+
 
 void setup()
 {
-    Serial.begin(9600); //* Start Serial communication
-    SystemConfig::get().begin();       //* Initializes the system
-    Serial.println("Waiting for all systems to initialize...");
-    
-    //* Small delay to ensure everything is set up
-    delay(2000);
-
-    //* Indicate initialization complete
-    Serial.println("Initialization complete.");
-    Serial.println("\n=== Plant Tracker ==="); //* Display in the monitor the Plant Tracker
+    Serial.begin(9600);
+    SystemConfig::get().begin(); // Initializes the system
 }
 
 void loop()
 {
-    //? Run the WiFi manager update
+    // Run Wifi updates 
     wifi.update();
-    //* String temp = sensors.getTemp();
-    //* String Humi = sensors.getHumi();
-    //* String moist = sensors.getSoilMoistData(1);
-    //* Serial.println("moist:");
-    //* Serial.println(moist);
-    //* Serial.println("temp:");
-    //* Serial.println(temp);
-    //* Serial.println("humi:");
-    //* Serial.println(Humi);
-    //* bool isNight = sensors.isNightDetected();
-    //* bool isRain = sensors.isRainDetected();
-    //* Serial.println(isRain ? "Raining" : "Clear");
-    //* Serial.println(isNight);
-    //* Add your sensor reading logic here
-    delay(1000);
+
+    // Run main logic
+    logic.runLogic();
+    
 }
