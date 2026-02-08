419cb8084ac3afba1786259de2180ae37513d832
carldev14 <carldev14.ac@gmail.com>
2026-02-08 17:00:48 +0800

    fix stepper motor not changing the signal to wake up the motor driver

--- src/actuators/stepper_motor.cpp
+++ src/actuators/stepper_motor.cpp
@@ -7,33 +7,36 @@ bool closingState = false;
 void ActuatorsManager::stepperBegin()
 {
     digitalWrite(SystemConfig::SLEEP_PIN, LOW); //* Disable the motor driver by default
-    stepper.setMaxSpeed(1000);                  // Higher steps/sec since steps are smaller
+    stepper.setMaxSpeed(1000);                   // Higher steps/sec since steps are smaller
     stepper.setAcceleration(400);
 }
 
 void ActuatorsManager::openCover()
 {
     digitalWrite(SystemConfig::SLEEP_PIN, HIGH); //* Enable the motor driver
-
-    // Move 5 full revolutions (5 * 200 steps = 1000 steps)
     stepper.move(5000);
-    digitalWrite(SystemConfig::SLEEP_PIN, LOW);
+    // DON'T set to LOW here - keep it enabled for runStepper()
 }
 
 void ActuatorsManager::closeCover()
 {
     digitalWrite(SystemConfig::SLEEP_PIN, HIGH); //* Enable the motor driver
-
-    // Move 5 revolutions in opposite direction
     stepper.move(-5000);
-    digitalWrite(SystemConfig::SLEEP_PIN, LOW);
+    // DON'T set to LOW here - keep it enabled for runStepper()
 }
 
 void ActuatorsManager::runStepper()
 {
+    // Keep SLEEP_PIN HIGH during movement
+    digitalWrite(SystemConfig::SLEEP_PIN, HIGH);
+    
     // Non-blocking movement toward target
     stepper.run();
     stepper.runToPosition(); // Blocks until movement is complete
+    
+    // Only disable after movement is complete
+    digitalWrite(SystemConfig::SLEEP_PIN, LOW);
+    
     delayMicroseconds(500);  // Add small delay between pulses
     stepper.stop();          // Stop the motor after reaching target
 }
