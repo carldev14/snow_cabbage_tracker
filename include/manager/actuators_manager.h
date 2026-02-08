81ff745965dd22eb508d6ca735900c7c1da9c798
carldev14 <carldev14.ac@gmail.com>
2026-02-08 16:07:52 +0800

    Removed some unnessary function: Instead we innovate things. We directly put the purpose of the relay.

--- include/manager/actuators_manager.h
+++ include/manager/actuators_manager.h
@@ -1,22 +1,27 @@
 #pragma once
 #include <Arduino.h>
-
+#include <config/system_config.h>
 
 class ActuatorsManager
 {
 public:
-    ActuatorsManager();  //* Constructor - NO return type
-    
+    ActuatorsManager(); //* Constructor - NO return type
+
     //* Initialize the ActuatorsManager
     void begin();
-    
+
     //* Relay
-    void useRelay(int pin, int value);
-    bool safeRelayOn(int pin, bool state);
-    void checkTimeouts();
+    void toggleArtificialLight(bool);
+    void toggleWaterPump(bool);
 
     //* Buzzer
     void runBuzzerPattern();
 
     // TODO: Add driver for stepper motor
+    //* With the stepper motor, we can control the opening and closing of a cover for the plant, which can help regulate sunlight and protect the plant from rain.
+    void openCover();
+    void closeCover();
+    void stepperBegin();
+    void stopCover();
+    void runStepper(); // Call this in loop() to keep the stepper moving toward its target
 };
