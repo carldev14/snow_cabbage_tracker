6347ed82e38f74b4e86be7f23ac6343d1fc44bac
carldev14 <carldev14.ac@gmail.com>
2026-02-09 21:42:37 +0800

    Add method to retrieve current system time in HHMM format

--- include/manager/system/wifi_manager.h
+++ include/manager/system/wifi_manager.h
@@ -22,11 +22,28 @@ public:
 
     //* Get the RSSI (signal strength)
     int getRSSI() const;
+
     void debugNetworkInfo();
+
+    /**
+     * @brief Get the current system time in HHMM format.
+     *
+     * Note: The time is based on military time.
+     * - Hours are from 0 to 23.
+     * - Returns a String in HHMM format (e.g., "1905" for 7:05 PM)
+     * - Updates only every TIME_INTERVAL milliseconds for performance.
+     *
+     * @return String Current time as HHMM.
+     */
+    String getRealTime();
+
 private:
     const char *_ssid;
     const char *_password;
     bool _connected;
+    unsigned long _lastTimeUpdate = 0;
+    const unsigned long TIME_INTERVAL = 1000 * 60 * 3; // 5 seconds
+    String _cachedTime = "----";
     unsigned long _lastAttemptTime;
     int _attemptCount;
 
