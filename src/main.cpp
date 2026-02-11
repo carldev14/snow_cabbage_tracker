d27ae87cfb677a78cee969851df3b20086040da2
carldev14 <carldev14.ac@gmail.com>
2026-02-11 10:42:14 +0800

    Reorganize setup and loop functions for improved readability and maintainability

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include "manager/logic_manager.h"
 #include "config/system_config.h"
-
+#include "Esp.h"
 LogicManager logic;
 
 void setup()
 {
   Serial.begin(9600);
+  ESP.getFreeHeap();           // show the memory usages
   SystemConfig::get().begin(); // Initializes the system
 }
 
 void loop()
 {
-  // Run main logic
-  logic.runLogic();
 
+    // Run Wifi updates
+    wifi.update();
+
+    // Run main logic
+    logic.runLogic();
 
 }
